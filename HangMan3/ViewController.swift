0b86f5e590a10011ebebfe8f9e2735e976f4130d
cdaly1217 <cdaly342@gmail.com>
2019-04-05 13:31:52 -0400

    Initial Commit

---
+++ HangMan3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HangMan3
+//
+//  Created by  on 4/5/19.
+//  Copyright © 2019 Daly's Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
