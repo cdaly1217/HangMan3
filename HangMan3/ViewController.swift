85aa2983affb89532ce1c2764252d054674f1ecb
cdaly1217 <cdaly342@gmail.com>
2019-04-08 13:31:44 -0400

    update letters

--- HangMan3/ViewController.swift
+++ HangMan3/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController
     
     var counts = 0
     var test = "Testing"
-    var alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
     
     
